ec07eec78682aa55ecb7d705fc580132d082d6bb
Devansh Ruia <133739735+Devansh-Ruia@users.noreply.github.com>
2024-09-15 13:22:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent! Now, I’ll get back to you once I done killing this Sekiro boss.');
+});
